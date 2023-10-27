3ebe103a09362bd28d74c752b9fa8f2585737a64
viquise <48965121+viquise@users.noreply.github.com>
2023-10-27 17:02:44 +0100

    Update

--- game.js
+++ game.js
@@ -1,49 +1,53 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
-var randomChosenColour = buttonColours[nextSequence()];
-var gamePattern= [];
-gamePattern.push(randomChosenColour);
+var gamePattern = []
 var userClickedPattern = [];
+var started = false;
+var level = 0;
 
 
-var chosenColor = $(".btn").click(function() {
-    var userChosenColor = $(this).attr("id");
+function nextSequence(){
+    var randomNumber = (Math.floor(Math.random()*4) + 1) - 1;
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).delay(100).fadeOut().fadeIn("slow");
+    playSound(randomChosenColour);
+    if (started == true){
+        $("#level-title").text("Level "+ level);
+    }
+
+}
+
+$(".btn").on('click', function(){
+    userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern);
+    animatePress(userChosenColor);
+    playSound(userChosenColor);
+
 });
 
-// Created a funcion to generate a random number from 0 to 3
-function nextSequence(){
-    var randomNumber = Math.floor(Math.random() * 4) + 1;
-    randomNumber -= 1;
-    return randomNumber
-};
-console.log(chosenColor);
-console.log(gamePattern);
-
-// Animating the Button
-$("#"+randomChosenColour).delay(100).fadeOut().fadeIn('slow');
-
-
-// Adding sounds to the Button
-switch(randomChosenColour){
-    case "red":
-        var red = new Audio("./sounds/red.mp3");
-        red.play();
-    break;
-    case "green":
-        var green = new Audio("./sounds/green.mp3");
-        green.play();
-    break;
-    case "blue":
-        var blue = new Audio("./sounds/blue.mp3");
-        blue.play();
-    break;
-    case "yellow":
-        var yellow = new Audio("./sounds/yellow.mp3");
-        yellow.play();
-    break;
-    default:
-        var wrong = new Audio("./sounds/wrong.mp3");
-        wrong.play();
-    break;
+function playSound(name){
+    var audio = new Audio("./sounds/"+name+".mp3");
+    audio.play();
 }
+
+function animatePress(currentColor){
+    $("."+currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("."+currentColor).removeClass("pressed");
+    }, 300)
+}
+
+
+// nextSequence();
+$(document).on('keyup', function(){
+    started = true;
+    if (started){
+        nextSequence();
+        started = false;
+    }
+});
+
+// while (started==true) {
+//     nextSequence();
+// }
+
